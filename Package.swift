07e49269f27a438fafda441b2edbd2dfed74475a
lockcept <lockcept@gmail.com>
2022-07-06 16:47:16 +0900

    chore: upgrade WebRtc package

--- Package.swift
+++ Package.swift
@@ -12,10 +12,10 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "PagecallSDK",
-            targets: ["PagecallSDK"]),
+            targets: ["PagecallSDK"])
     ],
     dependencies: [
-        .package(url: "https://github.com/alexpiezo/WebRTC.git", .upToNextMajor(from: "1.1.31567"))
+        .package(url: "https://github.com/alexpiezo/WebRTC.git", .upToNextMajor(from: "95.4638.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -28,6 +28,5 @@ let package = Package(
             ]),
         .testTarget(
             name: "PagecallSDKTests",
-            dependencies: ["PagecallSDK"]),
-    ]
-)
+            dependencies: ["PagecallSDK"])
+    ])
