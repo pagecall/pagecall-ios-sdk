fe271f6e10c01967ff2a6e4e8fbf85d81b28611d
lockcept <lockcept@gmail.com>
2022-07-26 21:21:53 +0900

    feat: swift message handler POC

--- Sources/PagecallSDK/NativeBridge.swift
+++ Sources/PagecallSDK/NativeBridge.swift
@@ -9,9 +9,57 @@ import Foundation
 import WebKit
 
 class NativeBridge {
-    init() {}
+    let webview: WKWebView
 
-    func messageHandler(message: String, evaluateJavaScript: (String) -> Void) {
-        print(message)
+    init(webview: WKWebView) { self.webview = webview }
+
+    func emit(eventName: String, data: Data) {
+        let string = String(data: data, encoding: .utf8)
+        if let string = string {
+            self.webview.evaluateJavaScript("window.PagecallNative.emit('\(eventName)','\(string)')")
+        }
+    }
+
+    func messageHandler(message: String) {
+        let data = message.data(using: .utf8)!
+
+        do {
+            guard let jsonArray = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any]
+            else {
+                NSLog("Failed to JSONSerialization")
+                return
+            }
+            guard let action = jsonArray["action"] as? String, let requestId = jsonArray["requestId"] as? String? else {
+                return
+            }
+
+            func response(data: Data) {
+                guard let requestId = requestId else {
+                    return
+                }
+                let string = String(data: data, encoding: .utf8)
+                if let string = string {
+                    self.webview.evaluateJavaScript("window.PagecallNative.response('\(requestId)','\(string)')")
+                }
+            }
+
+            switch action {
+            case "pauseAudio":
+                print("pause audio")
+            case "resumeAudio":
+                print("resume audio")
+            case "setAudioDevice":
+                print("set audio device")
+            case "getAudioDevices":
+                print("get audio devices")
+                let data = try JSONSerialization.data(withJSONObject: [])
+                response(data: data)
+            default:
+                break
+            }
+
+        } catch let error as NSError {
+            print(error)
+        }
     }
 }
