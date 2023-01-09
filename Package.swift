7ea9ac4c80e96ea27eebc78acf99a3c07d0e869a
lockcept <lockcept@gmail.com>
2023-01-09 22:33:47 +0900

    chore: change module name as PagecallCore

--- Package.swift
+++ Package.swift
@@ -4,21 +4,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "PagecallSDK",
+    name: "Pagecall",
     platforms: [
         .iOS(.v11)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "PagecallSDK",
-            targets: ["PagecallSDK", "AmazonChimeSDK", "AmazonChimeSDKMedia"])
+            name: "PagecallCore",
+            targets: ["PagecallCore", "AmazonChimeSDK", "AmazonChimeSDKMedia"])
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "PagecallSDK",
+            name: "PagecallCore",
             path: "Sources/PagecallSDK",
             resources: [
                 .process("PagecallNative.js")
@@ -31,5 +31,5 @@ let package = Package(
             path: "Binaries/AmazonChimeSDKMedia.xcframework"),
         .testTarget(
             name: "PagecallSDKTests",
-            dependencies: ["PagecallSDK"])
+            dependencies: ["PagecallCore"])
     ])
