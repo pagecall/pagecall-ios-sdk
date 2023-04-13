deef91c4def8bd2ec74d7fab9ca1105f75570e3e
Jaeseong Seo <07js23@gmail.com>
2023-04-13 17:32:03 +0900

    chore: route logs

--- Sources/PagecallSDK/ChimeController.swift
+++ Sources/PagecallSDK/ChimeController.swift
@@ -8,6 +8,46 @@
 import AmazonChimeSDK
 import AVFoundation
 
+class ChimeLogger: Logger {
+    private let consoleLogger = ConsoleLogger(name: "DefaultMeetingSession", level: LogLevel.INFO)
+    private let emitter: WebViewEmitter
+
+    init(emitter: WebViewEmitter) {
+        self.emitter = emitter
+    }
+
+    func `default`(msg: String) {
+        consoleLogger.default(msg: msg)
+    }
+
+    func debug(debugFunction: () -> String) {
+        consoleLogger.debug(debugFunction: debugFunction)
+    }
+
+    func info(msg: String) {
+        consoleLogger.info(msg: msg)
+    }
+
+    func fault(msg: String) {
+        consoleLogger.fault(msg: msg)
+        emitter.error(name: "ChimeFault", message: msg)
+    }
+
+    func error(msg: String) {
+        consoleLogger.fault(msg: msg)
+        emitter.error(name: "ChimeError", message: msg)
+    }
+
+    func setLogLevel(level: AmazonChimeSDK.LogLevel) {
+        consoleLogger.setLogLevel(level: level)
+    }
+
+    func getLogLevel() -> AmazonChimeSDK.LogLevel {
+        return consoleLogger.getLogLevel()
+    }
+
+}
+
 class ChimeController: MediaController {
     let emitter: WebViewEmitter
 
@@ -21,8 +61,7 @@ class ChimeController: MediaController {
     init(emitter: WebViewEmitter, configuration: MeetingSessionConfiguration) {
         self.emitter = emitter
 
-        let logger = ConsoleLogger(name: "DefaultMeetingSession", level: LogLevel.INFO)
-        meetingSession = DefaultMeetingSession(configuration: configuration, logger: logger)
+        meetingSession = DefaultMeetingSession(configuration: configuration, logger: ChimeLogger(emitter: emitter))
 
         realtimeObserver = ChimeRealtimeObserver(emitter: emitter, myAttendeeId: configuration.credentials.attendeeId)
         audioVideoObserver = ChimeAudioVideoObserver(emitter: emitter)
