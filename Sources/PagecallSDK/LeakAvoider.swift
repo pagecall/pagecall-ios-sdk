a0d09b82d812ce284d4ace4bc4a1b65e6a9a08c5
Jaeseong Seo <07js23@gmail.com>
2023-04-14 16:11:52 +0900

    refactor: factor out LeakAvoider

---
+++ Sources/PagecallSDK/LeakAvoider.swift
@@ -0,0 +1,14 @@
+import WebKit
+
+class LeakAvoider: NSObject, WKScriptMessageHandler {
+    weak var delegate: WKScriptMessageHandler?
+    init(delegate: WKScriptMessageHandler) {
+        self.delegate = delegate
+        super.init()
+    }
+    func userContentController(_ userContentController: WKUserContentController,
+                               didReceive message: WKScriptMessage) {
+        self.delegate?.userContentController(
+            userContentController, didReceive: message)
+    }
+}
--- Sources/PagecallSDK/PagecallWebViewController.swift
+++ Sources/PagecallSDK/PagecallWebViewController.swift
@@ -1,19 +1,6 @@
 import UIKit
 import WebKit
 
-class LeakAvoider: NSObject, WKScriptMessageHandler {
-    weak var delegate: WKScriptMessageHandler?
-    init(delegate: WKScriptMessageHandler) {
-        self.delegate = delegate
-        super.init()
-    }
-    func userContentController(_ userContentController: WKUserContentController,
-                               didReceive message: WKScriptMessage) {
-        self.delegate?.userContentController(
-            userContentController, didReceive: message)
-    }
-}
-
 public protocol PagecallDelegate {
     func pagecallDidClose(_ controller: PagecallWebViewController)
     func pagecallDidLoad(_ controller: PagecallWebViewController)
