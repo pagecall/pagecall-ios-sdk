70426b61161d8067ffdbf259433ab13007f2f39d
Jaeseong Seo <07js23@gmail.com>
2023-04-18 17:33:12 +0900

    refactor: send and listen at view level

--- Sources/PagecallSDK/PagecallWebView.swift
+++ Sources/PagecallSDK/PagecallWebView.swift
@@ -35,60 +35,151 @@ public class PagecallWebView: WKWebView, WKScriptMessageHandler {
         self.init(frame: .zero, configuration: .init())
     }
 
-    override public init(frame: CGRect, configuration: WKWebViewConfiguration) {
+    var scriptPath = {
+        #if SWIFT_PACKAGE
+        return Bundle.module.path(forResource: "PagecallNative", ofType: "js")
+        #else
+        return Bundle.init(for: Self.self).path(forResource: "PagecallNative", ofType: "js")
+        #endif
+    }()
 
+    var safariUserAgent: String = {
+        let webkitVersion = "605.1.15"
+        let systemVersion = UIDevice.current.systemVersion.replacingOccurrences(of: ".", with: "_")
+
+        let systemFragment = UIDevice.current.userInterfaceIdiom == .phone ?
+            "Mozilla/5.0 (iPhone; CPU iPhone OS \(systemVersion) like Mac OS X)"
+            : "Mozilla/5.0 (Macintosh; Intel Mac OS X \(systemVersion))"
+        let webkitFragment = "AppleWebKit/\(webkitVersion) (KHTML, like Gecko)"
+        let versionFragment = "Version/\(systemVersion)"
+        let browserFragment = "Safari/\(webkitVersion)"
+
+        return [systemFragment, webkitFragment, versionFragment, browserFragment].joined(separator: " ")
+    }()
+
+    override public init(frame: CGRect, configuration: WKWebViewConfiguration) {
         configuration.mediaTypesRequiringUserActionForPlayback = []
         configuration.allowsInlineMediaPlayback = true
         configuration.suppressesIncrementalRendering = false
         configuration.applicationNameForUserAgent = "PagecallIos"
         configuration.allowsAirPlayForMediaPlayback = true
+        configuration.defaultWebpagePreferences.preferredContentMode = .mobile
+        configuration.limitsNavigationsToAppBoundDomains = true
 
-        if #available(iOS 13.0, *) {
-            configuration.defaultWebpagePreferences.preferredContentMode = .mobile
+        if let scriptPath = scriptPath, let bindingJS = try? String(contentsOfFile: scriptPath, encoding: .utf8) {
+            let script = WKUserScript(source: bindingJS, injectionTime: .atDocumentStart, forMainFrameOnly: false)
+            configuration.userContentController.addUserScript(script)
+        } else {
+            print("[PagecallWebView] Failed to add PagecallNative script")
         }
-        if #available(iOS 14.0, *) {
-            configuration.limitsNavigationsToAppBoundDomains = true
-        }
+
         super.init(frame: frame, configuration: configuration)
-
         self.allowsBackForwardNavigationGestures = false
-
-        if #available(iOS 15.0, *) {
-            let osVersion = UIDevice.current.systemVersion
-            self.customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/\(osVersion) Safari/605.1.15"
-        }
-        #if SWIFT_PACKAGE
-        if let path = Bundle.module.path(forResource: "PagecallNative", ofType: "js") {
-            if let bindingJS = try? String(contentsOfFile: path, encoding: .utf8) {
-                let script = WKUserScript(source: bindingJS, injectionTime: .atDocumentStart, forMainFrameOnly: false)
-                configuration.userContentController.addUserScript(script)
-            }
-        } else {
-            NSLog("Failed to add PagecallNative script")
-            return
-        }
-        #else
-        if let path = Bundle.init(for: Self.self).path(forResource: "PagecallNative", ofType: "js") {
-            if let bindingJS = try? String(contentsOfFile: path, encoding: .utf8) {
-                let script = WKUserScript(source: bindingJS, injectionTime: .atDocumentStart, forMainFrameOnly: false)
-                configuration.userContentController.addUserScript(script)
-            }
-        } else {
-            NSLog("Failed to add PagecallNative script")
-            return
-        }
-        #endif
+        self.customUserAgent = safariUserAgent
         configuration.userContentController.add(LeakAvoider(delegate: self), name: self.controllerName)
     }
 
+    private var callbacks = [String: (Any?) -> Void]()
+    public func getReturnValue(script: String, completion: @escaping (Any?) -> Void) {
+        let id = UUID().uuidString
+        callbacks[id] = completion
+        let returningScript =
+            """
+const callback = (value) => {
+  window.webkit.messageHandlers.\(controllerName).postMessage({
+    type: "return",
+    payload: {
+      id: "\(id)",
+      value
+    }
+  });
+}
+const result = \(script);
+
+if (result.then) {
+  result.then(callback);
+} else {
+  callback(result);
+}
+"""
+        evaluateJavascriptWithLog(script: returningScript)
+    }
+
+    private var subscribers = [String: (Any?) -> Void]()
+    private let subscriptionsStorageName = "__pagecallNativeSubscriptions"
+    public func subscribe(target: String, subscriber: @escaping (Any?) -> Void) -> () -> Void {
+        let id = UUID().uuidString
+        subscribers[id] = subscriber
+        let returningScript =
+            """
+const callback = (value) => {
+  window.webkit.messageHandlers.\(controllerName).postMessage({
+    type: "subscription",
+    payload: {
+      id: "\(id)",
+      value
+    }
+  });
+}
+const subscription = \(target).subscribe(callback);
+if (!window["\(subscriptionsStorageName)"]) window["\(subscriptionsStorageName)"] = {};
+window["\(subscriptionsStorageName)"]["\(id)"] = subscription;
+"""
+        evaluateJavascriptWithLog(script: returningScript)
+        return {
+            self.evaluateJavascriptWithLog(script: """
+window["\(self.subscriptionsStorageName)"][\(id)]?.unsubscribe();
+""")
+            self.subscribers.removeValue(forKey: id)
+        }
+    }
+
     public func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        switch message.name {
-        case self.controllerName:
-            if let body = message.body as? String {
-                self.nativeBridge?.messageHandler(message: body)
+        guard message.name == self.controllerName else { return }
+        if let body = message.body as? [String: Any] {
+            guard let type = body["type"] as? String, let payload = body["payload"] as? [String: Any], let id = payload["id"] as? String else { return }
+            switch type {
+            case "return":
+                guard let callback = callbacks[id] else { return }
+                callbacks.removeValue(forKey: id)
+                callback(payload["value"])
+            case "subscription":
+                guard let subscriber = subscribers[id] else { return }
+                subscriber(payload["value"])
+            default:
+                print("[PagecallWebView] Unknown message type: \(type)")
             }
-        default:
-            break
+            return
+        } else if let body = message.body as? String {
+            self.nativeBridge?.messageHandler(message: body)
+        }
+    }
+
+    public func sendMessage(message: String, completionHandler: ((Error?) -> Void)?) {
+        evaluateJavascriptWithLog(script:
+"""
+if (!window.Pagecall) return false;
+window.Pagecall.sendMessage("\(message.javaScriptString)");
+return true;
+"""
+        ) { result, error in
+            if let error {
+                completionHandler?(error)
+            } else if let success = result as? Bool, success {
+                completionHandler?(nil)
+            } else {
+                completionHandler?(PagecallError(message: "Not initialized"))
+            }
+        }
+    }
+
+    public func listenMessage(subscriber: @escaping (String) -> Void) -> () -> Void {
+        return subscribe(target: "PagecallUI.customMessage$") { payload in
+            if let payload = payload as? String {
+                subscriber(payload)
+            } else {
+                print("[PagecallWebView] Invalid or unsupported message")
+            }
         }
     }
 
