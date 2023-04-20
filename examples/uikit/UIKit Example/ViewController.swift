99f5d608578e0d5eb6e7a304284ad5cfccaa6fe3
Jaeseong Seo <07js23@gmail.com>
2023-04-20 19:33:04 +0900

    docs: add uikit example

---
+++ examples/uikit/UIKit Example/ViewController.swift
@@ -0,0 +1,26 @@
+import UIKit
+import PagecallCore
+
+struct EmojiMessage: Codable {
+    let emoji: String
+    let sender: String
+}
+
+class ViewController: UIViewController, PagecallDelegate {
+    func pagecallDidClose(_ controller: PagecallCore.PagecallWebViewController) {
+        controller.dismiss(animated: true)
+    }
+
+    @IBOutlet weak var roomIdField: UITextField!
+
+    @IBAction func enterTapped(_ sender: Any) {
+        guard let roomId = roomIdField.text else { return }
+        if roomId.isEmpty { return }
+
+        let pagecallWebViewController = PagecallWebViewController()
+        pagecallWebViewController.delegate = self
+        _ = pagecallWebViewController.load(roomId: roomId, mode: .meet)
+        pagecallWebViewController.modalPresentationStyle = .fullScreen
+        present(pagecallWebViewController, animated: true)
+    }
+}
