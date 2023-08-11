d7032fa0b6d8ce5737d15dd7fd0534bab117a1e7
박주렁 <jurung@pplink.net>
2023-08-11 15:09:05 +0900

    release: 0.0.16

--- Sources/PagecallSDK/PagecallWebView.swift
+++ Sources/PagecallSDK/PagecallWebView.swift
@@ -57,7 +57,7 @@ extension WKWebView {
 }
 
 open class PagecallWebView: WKWebView {
-    static let version = "0.0.15"
+    static let version = "0.0.16"
 
     var nativeBridge: NativeBridge?
     var controllerName = "pagecall"
