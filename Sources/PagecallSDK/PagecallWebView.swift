b92afb4824dc2b9cbe51da7051955e7908eb2ecd
Jaeseong Seo <07js23@gmail.com>
2023-11-15 17:03:33 +0900

    release: 0.0.20

--- Sources/PagecallSDK/PagecallWebView.swift
+++ Sources/PagecallSDK/PagecallWebView.swift
@@ -57,7 +57,7 @@ extension WKWebView {
 }
 
 open class PagecallWebView: WKWebView {
-    static let version = "0.0.19"
+    static let version = "0.0.20"
 
     var nativeBridge: NativeBridge?
     var controllerName = "pagecall"
