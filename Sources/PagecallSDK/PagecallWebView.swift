9e6758edd08acf778cf8f8b47950018506c44b59
Jaeseong Seo <07js23@gmail.com>
2025-04-15 12:02:01 +0900

    release: 0.0.29

--- Sources/PagecallSDK/PagecallWebView.swift
+++ Sources/PagecallSDK/PagecallWebView.swift
@@ -55,7 +55,7 @@ public enum PagecallMode {
 let debugPencil = false
 
 open class PagecallWebView: WKWebView {
-    static let version = "0.0.28"
+    static let version = "0.0.29"
 
     var nativeBridge: NativeBridge?
     var controllerName = "pagecall"
